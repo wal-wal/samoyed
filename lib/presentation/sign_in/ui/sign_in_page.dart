d875894baf6f1c86d0a164f8d750c9d4d2a6af08
yoochanhong <chanhongy6@naver.com>
2024-06-08 20:39:29 +0900

    키보드 오버플로우 방지

--- lib/presentation/sign_in/ui/sign_in_page.dart
+++ lib/presentation/sign_in/ui/sign_in_page.dart
@@ -13,38 +13,45 @@ class SignInPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width - 12.0,
+            height: MediaQuery.of(context).size.height - 24.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const SizedBox(height: 150.0),
-                Image.asset(
-                  "assets/samoyed_logo.jpg",
-                  width: 80,
+                Column(
+                  children: [
+                    const SizedBox(height: 150.0),
+                    Image.asset(
+                      "assets/samoyed_logo.jpg",
+                      width: 80,
+                    ),
+                    const SizedBox(height: 50.0),
+                    SamoyedTextField.id(controller: idController),
+                    const SizedBox(height: 10.0),
+                    SamoyedTextField.password(controller: passwordController),
+                    const SizedBox(height: 50.0),
+                    const SamoyedButton(
+                      child: Text(
+                        "확인",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    SamoyedTextButton(
+                        onPressed: () {}, text: "회원가입을 하지 않으셨나요?"),
+                  ],
                 ),
-                const SizedBox(height: 50.0),
-                SamoyedTextField.id(controller: idController),
-                const SizedBox(height: 10.0),
-                SamoyedTextField.password(controller: passwordController),
-                const SizedBox(height: 50.0),
-                const SamoyedButton(
-                  child: Text(
-                    "확인",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                SamoyedTextButton(onPressed: () {}, text: "회원가입을 하지 않으셨나요?"),
+                SamoyedTextButton(onPressed: () {}, text: "비회원으로 이용하기"),
               ],
             ),
-            SamoyedTextButton(onPressed: () {}, text: "비회원으로 이용하기"),
-          ],
+          ),
         ),
       ),
     );
